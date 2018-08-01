fa860be19c363864d88b7c79521a811d1354cd7d
Hayeon0824 <ha_eon0824@naver.com>
2018-08-01 16:56:58 +0900

    packing and unpacking example

---
+++ Python/packing_unpacking.py
@@ -0,0 +1,6 @@
+clovers = ('클로버1', '하트2', '클로버3')
+print(clovers)
+
+alice_blue = (248, 248, 255)
+r, g, b = alice_blue
+print("R: ", r, "G: ", g, "B: ", b)
---
+++ Python/packing_unpacking_ex.py
@@ -0,0 +1,7 @@
+dodo = 'red'
+alice = 'blue'
+print('dodo: ', dodo, ', alice: ', alice)
+dodo, alice = alice, dodo
+#alice와 dodo가 packing이 되면서 dodo 와 alice로 unpacking이 된 것
+
+print('dodo: ', dodo, ', alice: ', alice)
