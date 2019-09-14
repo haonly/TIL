e7410fe0408786fe6247bb87ced635a2d1883d56
Haonly <khayeon0824@gmail.com>
2019-09-14 23:20:30 +0900

    아직 해결 못함

---
+++ Algorithm/카카오 문제/2017-3번.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  0909_algorithm
+//
+//  Created by Hayeon Kim on 04/09/2019.
+//  Copyright © 2019 Hayeon Kim. All rights reserved.
+//카카오 2017 3
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int solution(int cacheSize, string cities[]){
+    int time = 0;
+    int size = sizeof(cities);
+    return size;
+}
+
+int main(void) {
+    string arr[] = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
+    string arr1[] = {"Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"};
+    string arr2[] = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"};
+    string arr3[] = {"Jeju", "Pangyo", "NewYork", "newyork"};
+    string arr4[] = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
+    
+    cout << sizeof(arr3) <<endl;
+    
+    return 0;
+}
