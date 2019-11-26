fa37290e3e04722c1ae21ec037506c80e03902d7
Haonly <khayeon0824@gmail.com>
2019-11-26 13:48:17 +0900

    modify

--- Algorithm/DP/쉬운계단수.cpp
+++ Algorithm/DP/쉬운계단수.cpp
@@ -11,27 +11,23 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     int N;
     cin >> N;
-    vector<vector<int>> d(N, vector<int>(10, 0));
-    d[0] = {0, 1, 1 ,1 ,1 ,1 ,1 ,1 ,1 ,1};
+    int dp[101][10]= {};
+    int sum = 0;
     
-    for(int i =1; i<N; i++){
-        for(int digit = 0; digit<10; digit++){
-            if(digit == 0){
-                d[i][digit] = d[i-1][1];
-            }
-            else if(digit == 9){
-                d[i][digit] = d[i-1][8];
-            }
-            else{
-                d[i][digit] = (d[i-1][digit-1] + d[i-1][digit+1]) % DIV;
-            }
+    for(int i = 0; i < 10; i++)
+        dp[1][i] = 1;
+    for(int i = 2; i <= N; i++){
+        for(int j = 0; j < 10; j++){
+            if(j == 0)
+                dp[i][0] = dp[i-1][1] % DIV;
+            else if(j == 9)
+                dp[i][9] = dp[i-1][8] % DIV;
+            else
+                dp[i][j] = (dp[i-1][j+1] + dp[i-1][j-1]) % DIV;
         }
     }
-    int sum = 0;
-    for(int x : d[N-1]){
-        sum = (sum+x) % DIV;
-    }
-    
-    cout << sum << endl;
+    for(int i = 1; i < 10; i++)
+        sum = (sum + dp[N][i]) % DIV;
+    cout << sum % DIV << endl;
     return 0;
 }
