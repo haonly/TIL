2994fc1f814d4e587094b7f32b41df9eb81145a2
Haonly <khayeon0824@gmail.com>
2019-11-28 11:54:25 +0900

    modify

--- Algorithm/DP/쉬운계단수.cpp
+++ Algorithm/DP/쉬운계단수.cpp
@@ -1,5 +1,3 @@
-//10833 쉬운 계단 수(DP)
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -11,23 +9,25 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     int N;
     cin >> N;
-    int dp[101][10]= {};
-    int sum = 0;
+    int dp[101][10]= {}; //100자리까지 있고 마지막 자리는 0~9
+    long long sum = 0;
     
+    //1의 자리수는 1~9까지로 하나씩 있음
     for(int i = 0; i < 10; i++)
         dp[1][i] = 1;
+    
     for(int i = 2; i <= N; i++){
         for(int j = 0; j < 10; j++){
             if(j == 0)
-                dp[i][0] = dp[i-1][1] % DIV;
+                dp[i][0] = dp[i-1][1];
             else if(j == 9)
-                dp[i][9] = dp[i-1][8] % DIV;
+                dp[i][9] = dp[i-1][8];
             else
                 dp[i][j] = (dp[i-1][j+1] + dp[i-1][j-1]) % DIV;
         }
     }
     for(int i = 1; i < 10; i++)
-        sum = (sum + dp[N][i]) % DIV;
+        sum = (sum + dp[N][i]);
     cout << sum % DIV << endl;
     return 0;
 }
