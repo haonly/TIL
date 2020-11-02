2a0ec25c5e73f859575bb464fceae19354c2fd8a
hayeon-kim <khayeon0824@gmail.com>
2020-11-02 22:42:55 +0900

    Add one more cpp algorithm

---
+++ Algorithm/여러가지문제/두개뽑아서더하기.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+    int tmp;
+    for(int i = 0; i < numbers.size(); i++){
+        for(int j = 0; j < numbers.size(); j++){
+            if(i == j)
+                continue;
+            else{
+                answer.push_back(numbers[i] + numbers[j]);
+            }
+        }
+    }
+    sort(answer.begin(), answer.end());
+    answer.erase(unique(answer.begin(), answer.end()), answer.end());
+    
+    return answer;
+}
